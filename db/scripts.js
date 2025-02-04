bdba1371735338e39716c906422b8de8ac18253c
Johan Mendez <johan.mendez@cetus.com.co>
2025-02-03 22:26:43 -0500

    merge

--- db/scripts.js
+++ db/scripts.js
@@ -1,5 +1,5 @@
 // Declaracion constantes
-db_name = "gestion_db";
+db_name = "db_gestion";
 
 // Declaracion collections
 cll_role = "cll_role";
@@ -8,6 +8,7 @@ cll_order = "cll_order";
 cll_order_detail = "cll_order_detail"
 
 // creacion de base de datos
+<<<<<<< Updated upstream
 db_config_server = db.getSiblingDB(dbNameDev);
 
 db_config_server.createUser({
@@ -21,3 +22,17 @@ db_config_server.createUser({
 db_name.createCollection(cll_device);
 db_name.createCollection(cll_order);
 db_name.createCollection(cll_order_detail);
+=======
+
+db_gestion = db.getSiblingDB(db_name);
+db_gestion.createUser({
+	user: "user_application",
+	pwd: "12345",
+	roles: [{ role: "readWrite", db: dbNameDev }],
+});
+
+//crear coleccion Roles
+
+db_gestion.createCollection(cll_role);
+cll_role = db_gestion.getCollection(cll_role);
+>>>>>>> Stashed changes
