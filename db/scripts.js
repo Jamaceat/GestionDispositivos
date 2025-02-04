ced1702fa419047a8a65df739f35fd356a6f905f
JanerBlanco <janer.blanco@cetus.com.co>
2025-02-03 22:23:09 -0500

    Add collections to db gestion_db

--- db/scripts.js
+++ db/scripts.js
@@ -1,13 +1,23 @@
 // Declaracion constantes
 db_name = "gestion_db";
 
+// Declaracion collections
 cll_role = "cll_role";
+cll_device = "cll_device";
+cll_order = "cll_order";
+cll_order_detail = "cll_order_detail"
 
 // creacion de base de datos
-
 db_config_server = db.getSiblingDB(dbNameDev);
+
 db_config_server.createUser({
 	user: "usercs",
 	pwd: "328Dc2f873Gd",
 	roles: [{ role: "readWrite", db: dbNameDev }],
 });
+
+
+// Creacion colecciones
+db_name.createCollection(cll_device);
+db_name.createCollection(cll_order);
+db_name.createCollection(cll_order_detail);
