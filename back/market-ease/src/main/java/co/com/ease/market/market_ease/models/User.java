96fb364b2bc4579e6c7548a38d52c2c3601d0732
Johan Mendez <johan.mendez@cetus.com.co>
2025-02-06 23:04:13 -0500

    arreglando error

--- back/market-ease/src/main/java/co/com/ease/market/market_ease/models/User.java
+++ back/market-ease/src/main/java/co/com/ease/market/market_ease/models/User.java
@@ -2,6 +2,6 @@ package co.com.ease.market.market_ease.models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = )
+@Document(collation = "user")
 public class User {
 }
