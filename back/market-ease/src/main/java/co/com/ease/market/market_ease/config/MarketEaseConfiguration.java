b856d6a8efd12b6dd190502c0afb1a1720157a0d
Janer Jose Blanco Martinez <janerjoseblancomartinez@gmail.com>
2025-02-07 09:36:18 -0500

    format document

--- back/market-ease/src/main/java/co/com/ease/market/market_ease/config/MarketEaseConfiguration.java
+++ back/market-ease/src/main/java/co/com/ease/market/market_ease/config/MarketEaseConfiguration.java
@@ -3,6 +3,7 @@ package co.com.ease.market.market_ease.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,12 +11,7 @@ public class MarketEaseConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.disable())
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
-
-
+        http.httpBasic(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
